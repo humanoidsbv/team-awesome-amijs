65da34f8eb27949b22036e8114a8e46b290f6931
amijs-94 <67008183+amijs-94@users.noreply.github.com>
2021-06-01 09:44:28 +0200

    Fix lointing issues previous commit.

--- src/components/shared/button/Button.stories.tsx
+++ src/components/shared/button/Button.stories.tsx
@@ -1,4 +1,5 @@
-// YourComponent.stories.tsx
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-props-no-spreading */
 
 import React, { ComponentProps } from "react";
 
@@ -6,16 +7,12 @@ import { Story, Meta } from "@storybook/react";
 
 import Button from "./Button";
 
-// 👇 This default export determines where your story goes in the story list
-
 export default {
   title: "Button",
   argTypes: {
     type: { options: ["Primary", "Secondary"], control: { type: "radio" } },
   },
 } as Meta;
-
-// 👇 We create a “template” of how args map to rendering
 
 const Template: Story<ComponentProps<typeof Button>> = (args) => <Button {...args} />;
 
